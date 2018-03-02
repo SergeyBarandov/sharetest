46d4cb7590fbf60cafb447cdc0cc228a76477872
Artem Pavlov <javase@inbox.ru>
2018-03-02 23:24:02 +0300

    Начало разработки сервера

---
+++ server/src/main/java/config/Settings.java
@@ -0,0 +1,8 @@
+package config;
+
+/**
+ * Created by Artem on 02.03.2018.
+ */
+public enum Settings {
+    PORT
+}
---
+++ server/src/main/java/server/Server.java
@@ -0,0 +1,7 @@
+package server;
+
+/**
+ * Created by Artem on 02.03.2018.
+ */
+public class Server {
+}
